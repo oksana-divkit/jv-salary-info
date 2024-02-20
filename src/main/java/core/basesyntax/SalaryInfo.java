6d4e989fc064df862f3e073ce7f2811dbef3c5ca
Oksana Miazina <oksana.myazina@gmail.com>
2024-02-20 16:42:22 +0200

    refactor: add constants

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -10,9 +10,10 @@ public class SalaryInfo {
     private static final int INDEX_OF_NAME = 1;
     private static final int INDEX_OF_HOURS = 2;
     private static final int INDEX_OF_RATE = 3;
+    private static final String DASH = " - ";
+    private static final String RECORD_SEPARATOR = " ";
     private static final String LINE_SEPARATOR = System.lineSeparator();
-
-    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         int[] salaries = new int[names.length];
@@ -22,28 +23,33 @@ public class SalaryInfo {
         LocalDate parsedDateTo = parseDate(dateTo);
 
         for (String record : data) {
-            String[] parsedRecord = record.split(" ");
+            String[] parsedRecord = record.split(RECORD_SEPARATOR);
             LocalDate date = parseDate(parsedRecord[INDEX_OF_DATE]);
             String name = parsedRecord[INDEX_OF_NAME];
+            int indexOfRow = namesList.indexOf(name);
+
             int hours = Integer.parseInt(parsedRecord[INDEX_OF_HOURS]);
             int rate = Integer.parseInt(parsedRecord[INDEX_OF_RATE]);
 
-            int indexOfRow = namesList.indexOf(name);
-
-            if (date.compareTo(parsedDateFrom) >= 0
-                    && date.compareTo(parsedDateTo) <= 0
-                    && indexOfRow != -1) {
+            if (indexOfRow != -1 && isDateBetween(date, parsedDateFrom, parsedDateTo)) {
                 salaries[indexOfRow] += hours * rate;
             }
         }
 
-        String header = "Report for period " + dateFrom + " - " + dateTo;
-
+        String header = getReportHeader(dateFrom, dateTo);
         return generateReport(header, names, salaries);
     }
 
     private LocalDate parseDate(String date) {
-        return LocalDate.parse(date, formatter);
+        return LocalDate.parse(date, FORMATTER);
+    }
+
+    private boolean isDateBetween(LocalDate date, LocalDate from, LocalDate to) {
+        return !date.isBefore(from) && !date.isAfter(to);
+    }
+
+    private String getReportHeader(String dateFrom, String dateTo) {
+        return "Report for period " + dateFrom + DASH + dateTo;
     }
 
     private String generateReport(String header, String[] names, int[] salaries) {
@@ -52,7 +58,7 @@ public class SalaryInfo {
         for (int i = 0; i < names.length; i++) {
             reportBuilder.append(LINE_SEPARATOR)
                     .append(names[i])
-                    .append(" - ")
+                    .append(DASH)
                     .append(salaries[i]);
         }
 
